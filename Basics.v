d897288fe8940be12fc423125f803cda9d817a04
Sergey Sinchuk <Sergey.Sinchuk@jetbrains.com>
2014-06-06 02:04:32 +0400

    Formalized equivalence relations

--- Basics.v
+++ Basics.v
@@ -1,36 +1,50 @@
 Require Import ssreflect.
 Require Import Arith List Setoid BinPos BinNat Ring Relations.
 
+(* Formalization of the notion of equivalence relation *)
+
+Inductive eqv {Z} (R : relation Z) : Z -> Z -> nat -> Prop:=
+  | Cl_base  : forall z1 z2, R z1 z2 -> eqv R z1 z2 0
+  | Cl_swap  : forall z1 z2 n, eqv R z1 z2 n -> eqv R z2 z1 (S n)
+  | Cl_step  : forall z1 z2 z3 n, eqv R z1 z2 n -> eqv R z2 z3 n -> eqv R z1 z3 (S n).
+
+Implicit Arguments Cl_swap [[Z] [R] [z1] [z2] [n]].
+Implicit Arguments Cl_base [[Z] [R] [z1] [z2]].
+Implicit Arguments Cl_step [[Z] [R] [z1] [z2] [z3] [n]].
+
+Lemma Reqv: forall {Z} {R : relation Z}, reflexive Z R -> 
+forall n {z1 z2}, R z1 z2 -> eqv R z1 z2 n.
+Proof. intros Z R Refl. induction n; intros. by apply Cl_base. intros. apply (@Cl_step Z R z1 z2).
+apply IHn; apply H. rewrite /reflexive in Refl. by move: (IHn _ _ (Refl z2)). Qed.
+
+Corollary eqv_plus: forall {Z} {R : relation Z}, reflexive Z R -> 
+forall n' {z1 z2 n}, eqv R z1 z2 n -> eqv R z1 z2 (n+n').
+Proof. intros Z R Refl. induction n'; intros. rewrite plus_0_r //.
+rewrite -plus_Snm_nSm /=. rewrite /reflexive in Refl.
+by move: (Cl_step (IHn' _ _ _ H) (Reqv Refl (n+n') (Refl z2))). Qed.
+
+Inductive eq {Z} (R : relation Z) : relation Z :=
+  | Cl: forall {n z1 z2}, eqv R z1 z2 n -> eq R z1 z2.
+
+Implicit Arguments Cl [[Z] [R] [n] [z1] [z2]].
+
+Lemma equiv_eq: forall Z R, reflexive Z R -> equiv Z (eq R).
+intros Z R Refl. rewrite /equiv /reflexive /transitive /symmetric. 
+intros; split; [| split]; intros. 
+ + by move: (Cl ((Reqv Refl 0) (Refl x))).
+ + inversion H. inversion H0. subst.
+   apply (eqv_plus Refl) with (n':=n) in H4.
+   rewrite plus_comm in H4. by move: (Cl (Cl_step (eqv_plus Refl n0 H1) H4)).
+ + inversion H. subst. by move: (Cl (Cl_swap H0)). Qed.
+
+(* Formalization of basic combinatorial group theory *)
+
 Definition word (X : Type) := list (X * bool).
 Inductive eqg {X : Type} : relation (word X) :=
  | Eq_nil  : eqg nil nil
  | Eq_ins  : forall (x : X) f y z, eqg y z -> eqg ((x, f) :: (x, negb f) :: y) z
  | Eq_cons : forall e y z, eqg y z -> eqg (e :: y) (e :: z).
 
-Inductive cl' {Z} (R : relation Z) : Z -> Z -> nat -> Prop:=
-  | Cl_base0 : forall z1 z2, R z1 z2 -> cl' R z1 z2 0
-  | Cl_step  : forall z1 z2 z3 n, cl' R z1 z2 n -> cl' R z2 z3 n -> cl' R z1 z3 (S n).
-
-Lemma increase_n1: forall Z (R : relation Z), reflexive Z R -> 
-forall n z1 z2, R z1 z2 -> cl' R z1 z2 n.
-intros Z R Refl. induction n; intros. by apply Cl_base0. intros. apply (Cl_step R z1 z2).
-apply IHn; apply H. rewrite /reflexive in Refl. by move: (IHn _ _ (Refl z2)). Qed.
-
-Corollary increase_n: forall Z (R : relation Z), reflexive Z R -> 
-forall z1 z2 n n', cl' R z1 z2 n -> cl' R z1 z2 (n+n').
-intros Z R Refl. induction n'. rewrite plus_0_r //.
-rewrite -plus_Snm_nSm /=. move => A. rewrite /reflexive in Refl. 
-by move: (Cl_step R _ _ z2 _ (IHn' A) (increase_n1 Z R Refl (n+n') z2 z2 (Refl z2))). Qed.
-
-Inductive cl {Z} (R : relation Z) : relation Z :=
-  | Cl: forall z1 z2 n, cl' R z1 z2 n -> cl R z1 z2.
-
-Lemma L1: forall Z R, reflexive Z R -> transitive Z (cl R).
-intros Z R Refl. rewrite /equiv /reflexive /transitive. 
-intros.
- + destruct H0. destruct H. 
-   apply increase_n with (n':=n0) in H0. apply increase_n with (n':=n) in H.
-   rewrite plus_comm in H. by move: (Cl _ _ _ _ (Cl_step _ _ _ _ _ H H0)). done. done. Qed.
 
 Inductive term (X : Type) :=
  | Id : term X
