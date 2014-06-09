6fd1a7420eb5c4426437327404fe57ce12792f84
Sergey Sinchuk <Sergey.Sinchuk@jetbrains.com>
2014-06-09 12:19:30 +0400

    Formalizing basic CGT 4

--- Basics.v
+++ Basics.v
@@ -58,22 +58,45 @@ Proof. intros X. elim => [|[x f] ws IH] //=. by rewrite invw_app /= negb_involut
 
 Inductive eqg {X : Type} (P : word X -> Prop) : relation (word X) :=
  | Eq_nil  : eqg P nil nil
- | Eq_triv : forall x f w, eqg P ((x, f) :: (x, negb f) :: w) w
+ | Eq_triv : forall x f, eqg P [(x, f); (x, negb f)] nil
  | Eq_rel  : forall w, P w -> eqg P w nil
- | Eq_cons : forall e y z, eqg P y z -> eqg P (e :: y) (e :: z).
+ | Eq_consr: forall e y z, eqg P y z -> eqg P (y ++ [e]) (z ++ [e])
+ | Eq_consl : forall e y z, eqg P y z -> eqg P (e :: y) (e :: z).
 
 Implicit Arguments Eq_rel [[X] [P] [w]].
-Implicit Arguments Eq_cons [[X] [P] [y] [z]].
+Implicit Arguments Eq_consr [[X] [P] [y] [z]].
+Implicit Arguments Eq_consl [[X] [P] [y] [z]].
 
 Lemma eqg_plus: forall {X} {P : word X -> Prop} e {y z}, eqg P y z -> eqg P (e ++ y) (e ++ z).
 Proof. move => X P. elim => [|e es IH] y z H //. rewrite -?app_comm_cons.
-by move: (Eq_cons e (IH y z H)). Qed.
+by move: (Eq_consl e (IH y z H)). Qed.
 
 Corollary eqg_same: forall X (P : word X -> Prop) y, eqg P y y.
 Proof. intros. rewrite -(app_nil_r y). by move: (eqg_plus y (Eq_nil P)). Qed.
 
 Corollary eqg_reflexive: forall {X} (P : word X -> Prop), reflexive (word X) (eqg P).
 Proof. rewrite /reflexive => X P x. apply eqg_same. Qed.
+
+Lemma eqv_consl: forall {X} {P : word X -> Prop} {n} e {y z},
+ eqv (eqg P) y z n -> eqv (eqg P) (e :: y) (e :: z) n.
+Proof. intros X P. induction n; intros; inversion_clear H; subst.
+ + by move: (Cl_base (Eq_consl e H0)).
+ + apply IHn with (e:=e) in H0. by move: (Cl_swap H0).
+ + apply IHn with (e:=e) in H0. apply IHn with (e:=e) in H1.
+   by move: (Cl_step H0 H1). Qed.
+
+Lemma eqv_consr: forall {X} {P : word X -> Prop} {n} e {y z},
+ eqv (eqg P) y z n -> eqv (eqg P) (y ++ [e]) (z ++ [e]) n.
+Proof. intros X P. induction n; intros; inversion_clear H; subst.
+ + by move: (Cl_base (Eq_consr e H0)).
+ + apply IHn with (e:=e) in H0. by move: (Cl_swap H0).
+ + apply IHn with (e:=e) in H0. apply IHn with (e:=e) in H1.
+   by move: (Cl_step H0 H1). Qed.
+
+Lemma Eq_triv': forall {X} (P : word X -> Prop) w x f,
+ eqg P ((x, f) :: (x, negb f):: w) w.
+Proof. intros X P. apply (rev_ind (fun w => forall x f, eqg P ((x, f) :: (x, negb f) :: w) w)); intros.
+apply Eq_triv. rewrite ?app_comm_cons. apply Eq_consr. apply H. Qed.
 
 Definition eqgg {X} (P : word X -> Prop) := eq (@eqg X P).
 
@@ -83,26 +106,58 @@ Proof. rewrite /eqgg => X P x y z A B.
 move: (equiv_eq (word X) (eqg P) (eqg_reflexive P)) => [A1 [A2 A3]]. 
 rewrite /transitive in A2. apply (A2 x y z). apply A. apply B. Qed.
 
-Lemma eqv_cons: forall {X} {P : word X -> Prop} {n} e {y z},
- eqv (eqg P) y z n -> eqv (eqg P) (e :: y) (e :: z) n.
-Proof. intros X P. induction n; intros; inversion_clear H; subst.
- + by move: (Cl_base (Eq_cons e H0)).
- + apply IHn with (e:=e) in H0. by move: (Cl_swap H0).
- + apply IHn with (e:=e) in H0. apply IHn with (e:=e) in H1.
-   by move: (Cl_step H0 H1). Qed.
+Corollary eqgg_sym: forall {X} (P : word X -> Prop) {x} {y}, 
+  eqgg P x y -> eqgg P y x.
+Proof. rewrite /eqgg => X P x y A. 
+move: (equiv_eq (word X) (eqg P) (eqg_reflexive P)) => [A1 [A2 A3]]. 
+rewrite /symmetric in A3. apply (A3 x y). apply A. Qed.
 
-Corollary eqgg_plus: forall {X} {P : word X -> Prop} e {y z},
+Lemma eqgg_plusl: forall {X} {P : word X -> Prop} e {y z},
   eqgg P y z -> eqgg P (e ++ y) (e ++ z).
 Proof. intros X P. induction e as [|e es]; intros.
  + by move: H.
  + rewrite -?app_comm_cons. apply IHes in H. inversion H. 
-   subst. by move: (Cl n (eqv_cons e H0)). Qed.
+   subst. by move: (Cl n (eqv_consl e H0)). Qed.
 
-Lemma eqg_inv: forall X (P : word X -> Prop) w, eqgg P (w ++ (invw w)) nil.
+Lemma eqgg_plusr: forall {X} {P : word X -> Prop} e {y z},
+  eqgg P y z -> eqgg P (y ++ e) (z ++ e).
+Proof. intros X P. apply (rev_ind (fun e => forall y z, eqgg P y z -> eqgg P (y ++ e) (z ++ e))); intros.
+ + rewrite ?app_nil_r. by move: H.
+ + rewrite ?app_assoc. apply H in H0. inversion H0. 
+   subst. by move: (Cl n (eqv_consr x H1)). Qed.
+
+Lemma eqgg_invr: forall {X} (P : word X -> Prop) w, eqgg P (w ++ (invw w)) nil.
 Proof. intros X P. apply (rev_ind (fun l => eqgg P (l ++ invw l) nil)).
  + by move: (Cl 0 (Cl_base (Eq_nil P))).
  + move => [x f] l. rewrite invw_app /=. apply (eqgg_trans P (l ++ invw l)).
-   rewrite -app_assoc /=. by move: (Cl 0 (Cl_base (eqg_plus l (Eq_triv P x f (invw l))))). Qed.
+   rewrite -app_assoc /=. by move: (Cl 0 (Cl_base (eqg_plus l (Eq_triv' P (invw l) x f)))). Qed.
+
+Corollary eqgg_app: forall X (P : word X -> Prop) w1 w2 w3 w4, 
+ eqgg P w1 w2 -> eqgg P w3 w4 -> eqgg P (w1 ++ w3) (w2 ++ w4).
+Proof. intros. move: (@eqgg_trans X P (w1 ++ w3) (w2 ++ w3) (w2 ++ w4) ((eqgg_plusr w3) H)) => H1.
+by move: (H1 (eqgg_plusl w2 H0)). Qed.
+
+Lemma eqgg_redr: forall {X} (P : word X -> Prop) w {x y},
+ eqgg P (x ++ w) (y ++ w) -> eqgg P x y.
+Proof. intros. apply (eqgg_plusr (invw w)) in H. rewrite -?app_assoc in H.
+move: (eqgg_plusl x (eqgg_invr P w)) => H1. rewrite app_nil_r in H1.
+move: (eqgg_plusl y (eqgg_invr P w)) => H2. rewrite app_nil_r in H2.
+apply eqgg_sym in H1. by move: (eqgg_trans P _ (eqgg_trans P _ H1 H) H2). Qed.
+
+Corollary eqgg_invl: forall {X} (P : word X -> Prop) w, eqgg P ((invw w) ++ w) nil.
+Proof. intros. apply (eqgg_redr P (invw w)). move: (eqgg_plusl (invw w) (eqgg_invr P w)).
+by rewrite app_nil_l app_nil_r -app_assoc. Qed.
+
+Lemma eqgg_redl: forall {X} (P : word X -> Prop) w {x y},
+ eqgg P (w ++ x) (w ++ y) -> eqgg P x y.
+Proof. intros. apply (eqgg_plusl (invw w)) in H. rewrite ?app_assoc in H.
+move: (eqgg_plusr x (eqgg_invl P w)) => H1. rewrite app_nil_l in H1.
+move: (eqgg_plusr y (eqgg_invl P w)) => H2. rewrite app_nil_l in H2.
+apply eqgg_sym in H1. by move: (eqgg_trans P _ (eqgg_trans P _ H1 H) H2). Qed.
+
+Lemma eqgg_invert: forall X (P : word X -> Prop) y z, eqgg P y z -> eqgg P (invw z) (invw y).
+Proof. intros. apply (eqgg_redl P z). apply (eqgg_trans P nil (eqgg_invr P z)).
+apply (eqgg_plusr (invw y)) in H. apply (eqgg_trans P _ (eqgg_sym P (eqgg_invr P y))). apply H. Qed.
 
 (***************************)
 
