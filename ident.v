01c0560dce9cd57d4db88d8987eb485cdb75f6a6
Sergey Sinchuk <Sergey.Sinchuk@jetbrains.com>
2014-09-17 05:29:13 +0400

    Axioms of Relative Steinberg group written down

--- ident.v
+++ ident.v
@@ -12,23 +12,9 @@ Import SteinbergGroup.
 
 Definition swap_neq {i j : nat}: i != j -> j != i. by rewrite eq_sym.  Defined.
 
-(* This statement is needed to get rid of proof-relevant nature of parameter "p" of Z *) 
-Axiom swapI : forall {i j : nat} (p : i != j), swap_neq (swap_neq p) = p.
-
 Axiom X: forall {i j : nat}, (i != j) -> R -> ZZ.
 
-Definition Z {i j : nat} (p : i != j) (r s : R) : ZZ := locked ((X p r) ^ (X (swap_neq p) s)).
-
-Lemma Zdef {i j} (p : i!= j) r s: (X (p) r) ^ (X (swap_neq p) s) = Z p r s. by unlock Z. Qed.
-
-Lemma Zdef'{i j} (p : i!= j) r s: (X (swap_neq p) r) ^ (X p s) = Z (swap_neq p) r s.
-by unlock Z; rewrite swapI. Qed.
-
 Axiom ST0: forall {i j : nat} (ij : (i != j)) r s, X ij (r + s) = X ij r .* X ij s.
-Axiom ST1: forall {i j k : nat} (ij : (i != j)) (ik : (i != k)) (jk : (j != k)) r s,
- [~ X ij r , X jk s ] = X ik (r * s).
-Axiom ST2: forall {i j k l : nat} (ij : (i != j)) (kl : (k != l)) r s, j != k -> i != l ->
- [~ X ij r, X kl s ] = Id.
 
 Lemma Xzero: forall {i j : nat} (ij : (i != j)), X ij 0 = Id.
 intros. apply (GCr (X ij 0)). by rewrite -{3}(plus_1_l 0) ST0 IdG. Qed.
@@ -38,6 +24,9 @@ intros. apply (GCr (X ij r)). by rewrite -ST0 IG (inv_l) Xzero. Qed.
 
 Lemma ST0'' {i j} a b (ij : i != j): X ij a ^ X ij b = X ij a.
 expand. by rewrite -ST0' -?ST0 plus_comm -plus_assoc inv_r plus_1_l. Qed.
+
+Axiom ST1: forall {i j k : nat} (ij : (i != j)) (ik : (i != k)) (jk : (j != k)) r s,
+ [~ X ij r , X jk s ] = X ik (r * s).
 
 Lemma ST1': forall {i j k : nat} (ij : (i != j)) (ik : (i != k)) (jk : (j != k)) r s,
  [~ X jk r , X ij s ] = X ik (- s * r).
@@ -51,6 +40,9 @@ Lemma ST1''2 {i j k} a b {ij : i != j} {ki : k != i} (kj : k != j):
 X ij a ^ X ki b = X kj (- (b * a)) .* X ij a.
 by rewrite conj_com -ST0' ST1 inv_mul. Qed.
 
+Axiom ST2: forall {i j k l : nat} (ij : (i != j)) (kl : (k != l)) r s, j != k -> i != l ->
+ [~ X ij r, X kl s ] = Id.
+
 Lemma ST2': forall {i j k} {ik : i != k} {jk : j != k} (ij : i != j) a b, X ik a ^ X jk b = X ik a.
 intros. rewrite conj_com -ST0' ST2. by rewrite IdG. exact (swap_neq ik). exact jk. Qed.
 
@@ -62,6 +54,21 @@ intros. rewrite conj_com -ST0' ST2. by rewrite IdG. exact (swap_neq kj). exact k
 
 Corollary ST2''_swap: forall {i j k} {ki : k != i} {kj : k != j} (ij : i != j) a b, X ki a .* X kj b = X kj b .* X ki a.
 intros. rewrite -{2}(ST2'' ij a b). by cancellate. Qed.
+
+(* Tits relative generators z_{ij}(a, r) *)
+
+Definition Z {i j : nat} (p : i != j) (r s : R) : ZZ := locked ((X p r) ^ (X (swap_neq p) s)).
+
+Axiom swapI : forall {i j : nat} (p : i != j), swap_neq (swap_neq p) = p.
+   (* This axiom is needed to get rid of proof-relevant nature of parameter "p" of Z *) 
+
+Lemma Zdef {i j} (p : i!= j) r s: (X (p) r) ^ (X (swap_neq p) s) = Z p r s. by unlock Z. Qed.
+
+Lemma Zdef'{i j} (p : i!= j) r s: (X (swap_neq p) r) ^ (X p s) = Z (swap_neq p) r s.
+by unlock Z; rewrite swapI. Qed.
+
+Lemma Zadd {i j} (ij : i != j) (r s t : R): Z ij (r+s) t = Z ij r t .* Z ij s t.
+unlock Z. by rewrite -mul_conj ST0. Qed. 
 
 Lemma Zinv {i j} (ij : i != j) (r s : R): Z ij (-r) s = (Z ij r s)^-1.
 by unlock Z; rewrite /conj ST0' ?GIM ?GA GII. Qed.
@@ -129,7 +136,7 @@ intros. move: (R1''' i j k a 1 b ij ik jk) => /=.
 by rewrite ?mul_1_r ?mul_1_l -/ji -/ki -/kj. Qed.
 
 Section S1.
-Context (i j k l : nat) (a b c d r s: R) (ij : i != j) (ik : i != k) (jk : j != k)
+Context (i j k l : nat) (a b c d : R) (ij : i != j) (ik : i != k) (jk : j != k)
                                       (kl : k != l) (il : i != l) (jl : j != l).
 Let ji := (swap_neq ij). Let ki := (swap_neq ik). Let kj := (swap_neq jk).
 Let lk := (swap_neq kl). Let li := (swap_neq il). Let lj := (swap_neq jl).
@@ -165,3 +172,45 @@ by rewrite /Z -lock -/ji -conj_mul swap_comm ST2 // IdG
            conj_mul (conj_com (X ij a)) -ST0' ST2// IdG Zdef. Qed.
 
 End S1.
+(* =========================================================== *)
+(*  Another Presentation for Relative Steinberg Group          *)
+(* =========================================================== *)
+
+Section RelSteinberg.
+
+Axiom Z': forall {i j : nat} (p : i != j) (r s : R), ZZ. (* Formal Generator *)
+
+Definition X' {i j : nat} (p : i != j) r := Z' p r (0).
+
+Context {i j k l : nat} (a b c d : R) (ij : i != j) (ik : i != k) (jk : j != k)
+                                      (kl : k != l) (il : i != l) (jl : j != l).
+Let ji := (swap_neq ij). Let ki := (swap_neq ik). Let kj := (swap_neq jk).
+Let lk := (swap_neq kl). Let li := (swap_neq il). Let lj := (swap_neq jl).
+
+Axiom Z0: Z' ij (a + b) c = Z' ij a c .* Z' ij b c.
+Axiom Z0': (Z' ij a b) ^ (X' ji c) = Z' ij a (b + c).
+
+Axiom Z1: Z' ik (a * b) c =
+  X' kj (- (c * a)) .* X' ij a .* Z' ij (- a) (- (b * c)) .* X' jk (b * c * a * b)
+  .* X' ik (a * b) .* X' ji (- (b * c * a * b * c)) .* X' ki (- (c * a * b * c))
+  .* Z' kj (c * a) (- b). 
+
+Axiom Z1': Z' ij a (b * c) = 
+   X' jk (- (b * c * a * b)) .* X' ik (a * b) .* (X' ji (- (b * c * a * b * c))
+.* X' ki (- (c * a * b * c)) .* Z' kj (- (c * a)) (- b)) .* Z' ik (- (a * b)) (- c) 
+.* X' kj (c * a) .* X' ij a.
+
+Axiom ZC1: (Z' ij a b) ^ (X ij c) = 
+X' ik ((c * b + 1) * a) .* X' jk (- (b * a))
+.* X' ki (- ((b * c + 1) * b * a * b)) .* X' ji (- (b * a * b)) .* Z' jk (b * a) (- (b * c + 1)) 
+.* X' kj (b * (c * b + 1) * a * (b * c + 1)) .* X' ij ((c * b + 1) * a * (b * c + 1)) .* Z' ik (- ((c * b + 1) * a)) (- b).
+
+Axiom ZC2: (Z' ij a b) ^ (X ji c) = Z' ij a (b + c).
+
+Axiom ZC3: (Z' ij a b) ^ (X jk c) = X' jk (- (b * a * c)) .* X' ik (a * c) .* Z' ij a b.
+
+Axiom ZC4: (Z' ij a b) ^ (X kj c) = X' ki (c * b * a * b) .* X' kj (c * b * a) .* Z' ij a b.
+
+Axiom ZC5: (Z' ij a b) ^ (X kl c) = Z' ij a b.
+
+End RelSteinberg.
